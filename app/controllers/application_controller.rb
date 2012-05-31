81c7736319964e66a7ac110e8e4e5674dfd58ffe
Stefano Canducci <steno.c@gmail.com>
2012-05-31 13:48:01 +0200

    Simple auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :require_http_basic_auth
+
+  def require_http_basic_auth
+    authenticate_or_request_with_http_basic do |login, password|
+      if login == "echoboard" && password == "echoboardpassword"
+        true
+      end
+    end
+  end
 end
