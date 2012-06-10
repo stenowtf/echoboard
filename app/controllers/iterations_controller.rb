2a2472f4c54cd33b4caf07179d5dd730871bb38c
Stefano Canducci <steno.c@gmail.com>
2012-06-10 11:49:34 +0200

    List of Iterations in Iteration#show

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -2,13 +2,9 @@ class IterationsController < ApplicationController
   # GET /projects/:project_id/iterations/:id(.:format)
   def show
     @project = Project.find(params[:project_id])
+    @iterations = @project.iterations
     @iteration = @project.iterations.find(params[:id])
     @stories_in_iteration = @iteration.stories_in_iterations
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @iteration }
-    end
   end
 
   # GET /projects/:project_id/iterations/new(.:format)
@@ -17,11 +13,6 @@ class IterationsController < ApplicationController
     @iterations = @project.iterations
     @last_iteration = @project.iterations.last
     @iteration = @project.iterations.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @iteration }
-    end
   end
 
   # GET /projects/:project_id/iterations/:id/edit(.:format)
@@ -38,14 +29,10 @@ class IterationsController < ApplicationController
     @iterations = @project.iterations
     @iteration = @project.iterations.new(params[:iteration])
 
-    respond_to do |format|
-      if @iteration.save
-        format.html { redirect_to project_url(@project), notice: 'Iteration was successfully created.' }
-        format.json { render json: @iteration, status: :created, location: @iteration }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @iteration.errors, status: :unprocessable_entity }
-      end
+    if @iteration.save
+      redirect_to project_url(@project), notice: "Iteration was successfully created."
+    else
+      render action: "new"
     end
   end
 
@@ -55,14 +42,10 @@ class IterationsController < ApplicationController
     @iterations = @project.iterations
     @iteration = @project.iterations.find(params[:id])
 
-    respond_to do |format|
-      if @iteration.update_attributes(params[:iteration])
-        format.html { redirect_to project_iteration_url(@project, @iteration), notice: 'Iteration was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @iteration.errors, status: :unprocessable_entity }
-      end
+    if @iteration.update_attributes(params[:iteration])
+      redirect_to project_iteration_url(@project, @iteration), notice: "Iteration was successfully updated."
+    else
+      render action: "edit"
     end
   end
 
@@ -72,9 +55,6 @@ class IterationsController < ApplicationController
     @iteration = @project.iterations.find(params[:id])
     @iteration.destroy
 
-    respond_to do |format|
-      format.html { redirect_to project_url(@project), notice: 'Iteration was successfully deleted.' }
-      format.json { head :no_content }
-    end
+    redirect_to project_url(@project), notice: "Iteration was successfully deleted."
   end
 end
