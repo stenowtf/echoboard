05a29c92a7c98981af11217634a2aa0b9c2bd9a7
Stefano Canducci <steno.c@gmail.com>
2012-07-15 14:22:49 +0200

    Update iteration points when a Story in destroyed

--- app/models/story.rb
+++ app/models/story.rb
@@ -7,8 +7,8 @@ class Story < ActiveRecord::Base
 
   default_scope :order => "name"
 
-  after_save    :update_project_total_points
-  after_destroy :update_project_total_points
+  after_save    :update_project_total_points, :update_iteration_current_points, :update_iteration_total_points
+  after_destroy :update_project_total_points, :update_iteration_current_points, :update_iteration_total_points
 
   private
 
@@ -18,5 +18,25 @@ class Story < ActiveRecord::Base
         value += Difficulty.find(story.difficulty_id).value
       end
       self.project.update_column(:total_points, value)
+    end
+
+    def update_iteration_current_points
+      value = 0
+      self.stories_in_iterations.where("done = ?", true).each do |done_story_in_iteration|
+        value += Difficulty.find(Story.find(done_story_in_iteration.story_id).difficulty_id).value
+      end
+      self.stories_in_iterations.each do |iteration|
+        iteration.update_column(:iteration_points, value)
+      end
+    end
+
+    def update_iteration_total_points
+      value = 0
+      self.stories_in_iterations.each do |story_in_iteration|
+        value += Difficulty.find(Story.find(story_in_iteration.story_id).difficulty_id).value
+      end
+      self.stories_in_iterations.each do |iteration|
+        iteration.update_column(:total_points, value)
+      end
     end
 end
