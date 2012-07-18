26a41680d31d5ec3ecf395cf842b8077bd43283d
Stefano Canducci <steno.c@gmail.com>
2012-07-18 21:57:31 +0200

    Fix iteration points in edit/delete story

--- app/models/story.rb
+++ app/models/story.rb
@@ -21,22 +21,30 @@ class Story < ActiveRecord::Base
     end
 
     def update_iteration_current_points
-      value = 0
-      self.stories_in_iterations.where("done = ?", true).each do |done_story_in_iteration|
-        value += Difficulty.find(Story.find(done_story_in_iteration.story_id).difficulty_id).value
-      end
-      self.stories_in_iterations.each do |story_in_iteration|
-        story_in_iteration.iteration.update_column(:iteration_points, value)
+      StoriesInIteration.where("done = ? and story_id = ?", true, self.id).each do |done_story_in_iteration|
+        update_other_iterations_current_points(done_story_in_iteration)
       end
     end
 
-    def update_iteration_total_points
+    def update_other_iterations_current_points(done_story_in_iteration)
       value = 0
-      self.stories_in_iterations.each do |story_in_iteration|
-        value += Difficulty.find(Story.find(story_in_iteration.story_id).difficulty_id).value
+      StoriesInIteration.where("iteration_id = ?", done_story_in_iteration.iteration_id).each do |done_other_story_in_iteration|
+        value += Difficulty.find(Story.find(done_other_story_in_iteration.story_id).difficulty_id).value
       end
-      self.stories_in_iterations.each do |story_in_iteration|
-        story_in_iteration.iteration.update_column(:total_points, value)
+      Iteration.find(done_story_in_iteration.iteration_id).update_column(:total_points, value)
+    end
+
+    def update_iteration_total_points
+      StoriesInIteration.where("story_id = ?", self.id).each do |story_in_iteration|
+        update_other_iterations_total_points(story_in_iteration)
       end
+    end
+
+    def update_other_iterations_total_points(story_in_iteration)
+      value = 0
+      StoriesInIteration.where("iteration_id = ?", story_in_iteration.iteration_id).each do |other_story_in_iteration|
+        value += Difficulty.find(Story.find(other_story_in_iteration.story_id).difficulty_id).value
+      end
+      Iteration.find(story_in_iteration.iteration_id).update_column(:total_points, value)
     end
 end
