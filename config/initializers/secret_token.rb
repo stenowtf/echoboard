0d42fc169bba1e07f67f0fa9f5135ec48b3f743e
Stefano Canducci <steno.c@gmail.com>
2012-12-30 12:28:14 +0100

    secret_token in ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Echoboard::Application.config.secret_token = '8f87630e74e3220bc57e1bc871700fd573f3179e097414cb4da487c774c5651413a8be34b051cc8221274ad0ab47078fcf370bc84154bdcbecda87c1c74fd28f'
+Echoboard::Application.config.secret_token = ENV['SECRET']
